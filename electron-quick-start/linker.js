bf0da7a40c5fd98518edc49bc9fe32c27cadad3a
Eesa Aamer <43073112+eesaaamer3@users.noreply.github.com>
2020-11-23 22:57:22 -0500

    Update linker.js

--- electron-quick-start/linker.js
+++ electron-quick-start/linker.js
@@ -41,28 +41,31 @@ function writePython() {
 
 
 function getData1() {
+   // Receives data from the first page of answers, which is then uploaded to the main answer key array
     i = 0
     var x = document.getElementsByClassName("first-input");
     Array.prototype.forEach.call(x, function(el) {
             Answers.push(x[i].value)
             i++
     });
-    console.log(Answers)
+    //console.log(Answers)
 }
 
 function getData2() {
+  // Receives data from the first page of answers, which is then uploaded to the main answer key array
     i = 0
     var x = document.getElementsByClassName("second-input");
     Array.prototype.forEach.call(x, function(el) {
             Answers.push(x[i].value)
             i++
     });
-    console.log(Answers)
+    //console.log(Answers)
 }
 
 
 
 function getData3() {
+  // Receives data from the first page of answers, which is then uploaded to the main answer key array
     i = 0
     var x = document.getElementsByClassName("third-input");
     Array.prototype.forEach.call(x, function(el) {
@@ -70,19 +73,30 @@ function getData3() {
             i++
     });
     console.log(Answers);
+  // Triggers the Python script function within "linker.js" after all data is uploaded by the user 
     writePython()
 }
 
 
 
+/* 
+
+Receives the amount of questions that exist on the first page, and then creates that amount of 
+submission fields for that page 
+
+*/
+
 function num_questions1() {
     let style = "margin-top: 5px; margin-bottom: 10px"
     var q = 1
+    // Receives all of the info given by the user
     let num = document.getElementById("hello1").value;
     let j = document.createElement("button");
     j.innerHTML = "Submit";
+    // Assigns a button with a function on that page to allow user data to be uploaded 
     j.setAttribute("onclick", "getData1();");
 
+    // Continously creates new submission fields until the amount meets the user requirement
     for (let i = 0; i< num; i++){ 
         let dad = document.getElementById("questions1");
             
@@ -111,16 +125,26 @@ function num_questions1() {
 
     document.getElementById("questions1").appendChild(j)
 }
+
+/* 
+
+Receives the amount of questions that exist on the second page, and then creates that amount of 
+submission fields for that page 
+
+*/
              
 function num_questions2() {
     let style = "margin-top: 5px; margin-bottom: 10px"
     var q = 1
+    // Receives all of the info given by the user
     let num = document.getElementById("hello2").value;
     let j = document.createElement("button");
     j.innerHTML = "Submit";
+    // Assigns a button with a function on that page to allow user data to be uploaded 
     j.setAttribute("onclick", "getData2();");
 
     for (let i = 0; i< num; i++){ 
+      // Continously creates new submission fields until the amount meets the user requirement
         let dad = document.getElementById("questions2");
             
         let i = document.createElement("input");
@@ -149,15 +173,25 @@ function num_questions2() {
     document.getElementById("questions2").appendChild(j)
 }
 
+/* 
+
+Receives the amount of questions that exist on the second page, and then creates that amount of 
+submission fields for that page 
+
+*/
+
 function num_questions3() {
     let style = "margin-top: 5px; margin-bottom: 10px"
     var q = 1
+    // Receives all of the info given by the user
     let num = document.getElementById("hello3").value;
     let j = document.createElement("button");
     j.innerHTML = "Submit";
+  // Assigns a button with a function on that page to allow user data to be uploaded 
     j.setAttribute("onclick", "getData3();");
 
     for (let i = 0; i< num; i++){ 
+      // Continously creates new submission fields until the amount meets the user requirement
         let dad = document.getElementById("questions3");
             
         let i = document.createElement("input");
